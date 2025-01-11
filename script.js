3e3794b9d1ac05fb3e635f18516e2f0b99190b05
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-12 00:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const p=document.getElementById("status")
+const btn=document.getElementById("enterBtn")
+
+btn.addEventListener('click',()=>{
+	// alert("e")
+	p.innerHTML='<h1>Enter the Metaverse</h1>'
+})
